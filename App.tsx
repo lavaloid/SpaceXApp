706aa122bfe82aeecb278f9effdd84d44f1f048c
lavaloid <36921951+lavaloid@users.noreply.github.com>
2021-11-27 23:32:02 +0800

    Create navigation stack

--- App.tsx
+++ App.tsx
@@ -1,7 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import Home from './screens/Home';
 
 // Initialize Apollo Client
 const client = new ApolloClient({
@@ -9,22 +12,16 @@ const client = new ApolloClient({
   cache: new InMemoryCache()
 });
 
+const Stack = createNativeStackNavigator();
+
 export default function App() {
   return (
     <ApolloProvider client={client}>
-      <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </ApolloProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
