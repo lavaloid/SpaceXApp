b78b611f1e71c6b7dfbcdcaece498e001b8d2101
lavaloid <36921951+lavaloid@users.noreply.github.com>
2021-11-28 15:28:10 +0800

    Add GraphQL functionality

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,18 +1,89 @@
 import * as React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, ActivityIndicator, ImageBackground, TextInput, FlatList } from 'react-native';
+import { gql, useQuery } from '@apollo/client';
+import { StatusBar } from 'expo-status-bar';
+import HomeContainer from '../components/HomeContainer';
+import RocketItem from '../components/RocketItem';
 
-export default function Home() {
-  return (
-    <View>
-      <Text>Hello world!</Text>
-    </View>
-  );
+const ROCKETS_QUERY = gql`
+    query GetRocketsOverview {
+        rockets {
+            id
+            country
+            name
+        }
+    }
+`;
+
+/**
+ * The content of the home screen. Includes loading and error handling.
+ */
+const HomeContent = () => {
+    const { data, error, loading } = useQuery(ROCKETS_QUERY);
+
+    if (loading) {
+        return (
+            <ActivityIndicator />
+        );
+    }
+
+    if (error) {
+        return (
+            <Text>{error.message}</Text>
+        );
+    }
+
+    return (
+        <FlatList
+            data={data.rockets}
+            renderItem={({ item }) => (
+                <RocketItem name={item.name} country={item.country} />
+        )}
+        keyExtractor={(item) => item.id.toString()}
+        style={styles.itemListContainer}
+      />
+    );
+};
+
+/**
+ * This function is what will be imported when Home.tsx is imported.
+ */
+export default function Home({ navigation }: { navigation: any }) {
+    return (
+        <HomeContainer>
+            <HomeContent />
+        </HomeContainer>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    /* GENERAL STYLESHEETS */
+    container: {
+        height: '100%',
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'flex-start',
+        flexDirection: 'column',
+    },
+    separator: {
+        marginVertical: 30,
+        height: 1,
+        width: '80%',
+    },
+
+    /* ITEM LIST STYLESHEETS */
+    itemListContainer: {
+        width: '100%',
+    },
+    itemContainer: {
+        paddingHorizontal: 15,
+        paddingVertical: 5,
+    },
+    itemTitle: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    itemContent: {
+
+    },
 });
