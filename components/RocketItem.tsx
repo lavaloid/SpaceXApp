0acb9deb02f472565c211f5891ff3c805f0a56c3
lavaloid <36921951+lavaloid@users.noreply.github.com>
2021-11-28 17:55:53 +0800

    Add '>' icon to the right of list items

--- components/RocketItem.tsx
+++ components/RocketItem.tsx
@@ -14,22 +14,31 @@ export default function RocketItem (props: any) {
             onTouchStart={() => {setIsTouched(true)}}
             onTouchEnd={() => {setIsTouched(false)}}
             onTouchCancel={() => {setIsTouched(false)}}>
-            <Text style={styles.itemTitle}>
-                {props.name}
-            </Text>
-            <Text style={styles.itemContent}>
-                {props.country}
-            </Text>
+            <View style={styles.itemContentContainer}>
+                <Text style={styles.itemTitle}>
+                    {props.name}
+                </Text>
+                <Text style={styles.itemContent}>
+                    {props.country}
+                </Text>
+            </View>
+            <Text style={styles.gotoIcon}>&gt;</Text>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
     itemContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
         paddingHorizontal: 20,
         paddingTop: 7,
         paddingBottom: 10,
         backgroundColor: '#fff',
+    },
+    itemContentContainer: {
+        flexGrow: 1,
     },
     itemTitle: {
         fontSize: 24,
@@ -37,5 +46,8 @@ const styles = StyleSheet.create({
     },
     itemContent: {
         fontSize: 16,
+    },
+    gotoIcon: {
+        fontSize: 45,
     },
 });
