b22dd628321e06ad6b840ea1877b6434ee55956a
lavaloid <36921951+lavaloid@users.noreply.github.com>
2021-11-28 15:24:24 +0800

    Create container for homepage content

---
+++ components/HomeContainer.tsx
@@ -0,0 +1,73 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, View, ImageBackground, Text, TextInput, useWindowDimensions } from "react-native";
+/**
+ * The header for the home screen. Contains the title card and a search bar.
+ * Also has a rocket launch as a background.
+ */
+const HomeHeader = () => {
+    const windowWidth = useWindowDimensions().width;
+    const windowHeight = useWindowDimensions().height;
+
+    return (
+        <View style={[styles.titleContainer,
+            {height: windowHeight * 0.42}]}>
+            <ImageBackground
+                source={require('../assets/images/title-header.jpg')}
+                style={styles.titleBackgroundImg}
+            >
+                <TextInput style={[styles.searchBar, 
+                    {width: windowWidth - 20}]} />
+                <Text style={styles.title}>Read about SpaceX rockets.</Text>
+            </ImageBackground>
+        </View>
+    );
+};
+
+/**
+ * Container for the home screen content.
+ */
+export default function HomeContainer (props: any) {
+    return (
+        <View style={styles.container}>
+            <HomeHeader />
+            {props.children}
+            <StatusBar style="auto" />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        height: '100%',
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'flex-start',
+        flexDirection: 'column',
+    },
+    title: {
+        color: 'white',
+        fontSize: 50,
+        fontWeight: 'bold',
+        marginHorizontal: 30,
+    },
+    titleBackgroundImg: {
+        width: '100%',
+        height: '100%',
+        top: 0,
+        left: 0,
+    },
+    titleContainer: {
+        width: '100%',
+        justifyContent: 'center',
+    },
+    searchBar: {
+        fontSize: 18,
+        borderRadius: 10,
+        backgroundColor: 'white',
+        paddingHorizontal: 15,
+        paddingVertical: 5,
+        margin: 10,
+        marginTop: 20,
+    },
+})
